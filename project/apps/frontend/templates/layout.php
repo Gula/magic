9e7a1e1aa2b4c029ca95b6104e32b55e1b3190eb
Laura <laura.melo@xifox.net>
2010-03-30 03:00:49 -0300

    Trabajando en el layout principal del frontend.

--- project/apps/frontend/templates/layout.php
+++ project/apps/frontend/templates/layout.php
@@ -8,7 +8,80 @@
     <?php include_stylesheets() ?>
     <?php include_javascripts() ?>
   </head>
+
   <body>
+    <div class="container">
+      <div id="header">
+        <div id="logo" class="span-5"><h1><a href="index.html">Casino Magic</a></h1></div>
+        <div id="nav" class="span-19 last">
+          <div class="span-9 prepend-10 last" id="global">Iconos Globales</div>
+          <div id="menu">
+            <ul class="menu">
+              <li id="hotel"><a href="hotel.html">Hotel</a></li>
+              <li id="casino"><a href="casino.html">Casino</a></li>
+              <li id="eventos"><a href="hotel.html">Eventos</a></li>
+              <li id="espectaculos"><a href="hotel.html">Espect&aacute;culos</a></li>
+              <li id="promociones"><a href="hotel.html">Promociones</a></li>
+              <li id="gastronomia"><a href="hotel.html">Gastronom&iacute;a</a></li>
+              <li id="giftshop"><a href="hotel.html">Gift Shop</a></li>
+              <li id="search"><label for="search"><input border="0" id="searchInput" /></label></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="span-18">
+      </div>
+      <div class="span-5 last">
+        <ul class="drawers">
+          <img src="images/titulo_agenda.png" width="232" height="34" alt="Agenda" />
+          <li class="drawer">
+            <h2 class="drawer-handle open"><img src="images/agenda_titulo_wisin_yandel.png" width="232" height="24" alt="Agenda" /></h2>
+            <ul>
+              <li class="drawer-foto"><img src="images/agenda_foto_wisin_yandel.jpg" width="200" height="231" /></li>
+              <li class="drawer-texto"><div><img src="images/agenda_texto_wisin_yandel.png" width="200" height="78" /></div></li>
+            </ul>
+          </li>
+          <li class="drawer">
+            <h2 class="drawer-handle open"><img src="images/agenda_titulo_wisin_yandel.png" width="232" height="24" alt="Agenda" /></h2>
+            <ul>
+              <li class="drawer-foto"><img src="images/agenda_foto_wisin_yandel.jpg" width="200" height="231" /></li>
+              <li class="drawer-texto"><div><img src="images/agenda_texto_wisin_yandel.png" width="200" height="78" /></div></li>
+            </ul>
+          </li>
+          <li class="drawer">
+            <h2 class="drawer-handle open"><img src="images/agenda_titulo_wisin_yandel.png" width="232" height="24" alt="Agenda" /></h2>
+            <ul>
+              <li class="drawer-foto"><img src="images/agenda_foto_wisin_yandel.jpg" width="200" height="231" /></li>
+              <li class="drawer-texto"><div><img src="images/agenda_texto_wisin_yandel.png" width="200" height="78" /></div></li>
+            </ul>
+          </li>
+          <li class="drawer">
+            <h2 class="drawer-handle open"><img src="images/agenda_titulo_wisin_yandel.png" width="232" height="24" alt="Agenda" /></h2>
+            <ul>
+              <li class="drawer-foto"><img src="images/agenda_foto_wisin_yandel.jpg" width="200" height="231" /></li>
+              <li class="drawer-texto"><div><img src="images/agenda_texto_wisin_yandel.png" width="200" height="78" /></div></li>
+            </ul>
+          </li>
+          <li class="drawer">
+            <h2 class="drawer-handle open"><img src="images/agenda_titulo_wisin_yandel.png" width="232" height="24" alt="Agenda" /></h2>
+            <ul>
+              <li class="drawer-foto"><img src="images/agenda_foto_wisin_yandel.jpg" width="200" height="231" /></li>
+              <li class="drawer-texto"><div><img src="images/agenda_texto_wisin_yandel.png" width="200" height="78" /></div></li>
+            </ul>
+          </li>
+          <li class="drawer last">
+            <h2 class="drawer-handle open"><img src="images/agenda_titulo_wisin_yandel.png" width="232" height="24" alt="Agenda" /></h2>
+            <ul>
+              <li class="drawer-foto"><img src="images/agenda_foto_wisin_yandel.jpg" width="200" height="231" /></li>
+              <li class="drawer-texto"><div><img src="images/agenda_texto_wisin_yandel.png" width="200" height="78" /></div></li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+      <hr />
+      <div>Pie del sitio</div>
+    </div>
+
     <?php echo $sf_content ?>
   </body>
 </html>
