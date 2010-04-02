bc9ced53d8a35ef2bd23d88bf0c4d4845aba6361
RetroFox <damian.suarez@xifox.net>
2010-04-02 18:14:59 -0300

    fixed bug por Slideshow fuera de la pagina principal

--- project/web/js/casino.js
+++ project/web/js/casino.js
@@ -10,43 +10,46 @@ window.addEvent('domready', function (ev) {
   });
 
   // Main Slideshow
-  var slideContainer = $('slideshow'),
-      slide_wrapper = slideContainer.getElement('.slide-wrapper'),
-      count_slides = slideContainer.getElements('.picture').length;
+  var slideContainer = $('slideshow');
 
-  slide_wrapper.setStyles({
-    'width': count_slides*720
-  });
+  if(slideContainer) {
+    var slide_wrapper = slideContainer.getElement('.slide-wrapper'),
+    count_slides = slideContainer.getElements('.picture').length;
 
-  var slide = new Fx.Scroll(slideContainer, {
-    wait: false,
-    duration: 1000,
-    transition: Fx.Transitions.Quad.easeInOut
-  });
+    slide_wrapper.setStyles({
+      'width': count_slides*720
+    });
 
-  // auto
-  var moveSlide = function (ind) {
-    if (ind===undefined) {
-      if(this.ind == undefined) this.ind = 0;
-      this.ind = (this.ind > (count_slides - 1)) ? 0 : this.ind;
+    var slide = new Fx.Scroll(slideContainer, {
+      wait: false,
+      duration: 1000,
+      transition: Fx.Transitions.Quad.easeInOut
+    });
+
+    // auto
+    var moveSlide = function (ind) {
+      if (ind===undefined) {
+        if(this.ind == undefined) this.ind = 0;
+        this.ind = (this.ind > (count_slides - 1)) ? 0 : this.ind;
+      }
+      else {
+        this.ind = ind;
+      }
+
+      slide.start(720*this.ind, 0);
+      this.ind++;
     }
-    else {
-      this.ind = ind;
-    }
 
-    slide.start(720*this.ind, 0);
-    this.ind++;
+    var timer = moveSlide.periodical(10000);
+
+    // slideshow index
+    var slideIndex = $('slide-index');
+    slideIndex.addEvents({
+      click: function (ev) {
+        timer = $clear(timer);
+        timer = moveSlide.periodical(10000);
+        moveSlide(ev.target.get('text').toInt() -1);
+      }
+    })
   }
-
-  var timer = moveSlide.periodical(10000);
-
-  // slideshow index
-  var slideIndex = $('slide-index');
-  slideIndex.addEvents({
-    click: function (ev) {
-      timer = $clear(timer);
-      timer = moveSlide.periodical(10000);
-      moveSlide(ev.target.get('text').toInt() -1);
-    }
-  })
 })
