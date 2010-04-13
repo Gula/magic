ad97d53ce9f25897c74c37f13a2abf658fc48cf4
Laura <laura.melo@xifox.net>
2010-04-13 15:08:23 -0300

    fix bug in children pages

--- project/apps/frontend/modules/pages/templates/_slide_element.php
+++ project/apps/frontend/modules/pages/templates/_slide_element.php
@@ -1,4 +1,6 @@
 <?php
+  $level = (isset($level)) ? $level : 1;
+
   if(is_file(sfConfig::get('sf_upload_dir').'/'.$page->get('id').'/img_'.$page->get('id').'_250x141.jpg')) {
     $img_url = '/uploads/'.$page->get('id').'/img_'.$page->get('id').'_250x141.jpg';
   }
@@ -8,7 +10,7 @@
 ?>
 
 <li class="subpagina">
-  <?php echo link_to(image_tag($img_url, array('class' => 'imagen-subpagina')), 'pages/index?id='.$page->getId().'&level=2') ?>
-  <p class="titulo-subpagina"><?php echo link_to($page, 'pages/index?id='.$page->getId().'&level=2') ?></p>
+  <?php echo link_to(image_tag($img_url, array('class' => 'imagen-subpagina')), 'pages/index?id='.$page->getId().'&level='.$level) ?>
+  <p class="titulo-subpagina"><?php echo link_to($page, 'pages/index?id='.$page->getId().'&level='.$level) ?></p>
   <span class="resumen-subpagina"><?php echo $page->getRawValue()->getDescriptionAbstract() ?></span>
 </li>
