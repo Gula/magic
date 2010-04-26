35738ae1b8d21b35050ac280de429eb1d237f98c
RetroFox <damian.suarez@xifox.net>
2010-04-26 13:15:39 -0300

    cambiando generator como plugin

--- project/apps/backend/modules/pages/actions/actions.class.php
+++ project/apps/backend/modules/pages/actions/actions.class.php
@@ -1,5 +1,8 @@
 <?php
 
+require_once dirname(__FILE__).'/../lib/pagesGeneratorConfiguration.class.php';
+require_once dirname(__FILE__).'/../lib/pagesGeneratorHelper.class.php';
+
 /**
  * pages actions.
  *
@@ -8,66 +11,6 @@
  * @author     Your name here
  * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
  */
-class pagesActions extends sfActions
+class pagesActions extends autoPagesActions
 {
-  public function executeIndex(sfWebRequest $request)
-  {
-    $this->pages = Doctrine::getTable('Page')
-      ->createQuery('a')
-      ->execute();
-  }
-
-  public function executeNew(sfWebRequest $request)
-  {
-    $this->form = new PageForm();
-  }
-
-  public function executeCreate(sfWebRequest $request)
-  {
-    $this->forward404Unless($request->isMethod(sfRequest::POST));
-
-    $this->form = new PageForm();
-
-    $this->processForm($request, $this->form);
-
-    $this->setTemplate('new');
-  }
-
-  public function executeEdit(sfWebRequest $request)
-  {
-    $this->forward404Unless($page = Doctrine::getTable('Page')->find(array($request->getParameter('id'))), sprintf('Object page does not exist (%s).', $request->getParameter('id')));
-    $this->form = new PageForm($page);
-  }
-
-  public function executeUpdate(sfWebRequest $request)
-  {
-    $this->forward404Unless($request->isMethod(sfRequest::POST) || $request->isMethod(sfRequest::PUT));
-    $this->forward404Unless($page = Doctrine::getTable('Page')->find(array($request->getParameter('id'))), sprintf('Object page does not exist (%s).', $request->getParameter('id')));
-    $this->form = new PageForm($page);
-
-    $this->processForm($request, $this->form);
-
-    $this->setTemplate('edit');
-  }
-
-  public function executeDelete(sfWebRequest $request)
-  {
-    $request->checkCSRFProtection();
-
-    $this->forward404Unless($page = Doctrine::getTable('Page')->find(array($request->getParameter('id'))), sprintf('Object page does not exist (%s).', $request->getParameter('id')));
-    $page->delete();
-
-    $this->redirect('pages/index');
-  }
-
-  protected function processForm(sfWebRequest $request, sfForm $form)
-  {
-    $form->bind($request->getParameter($form->getName()), $request->getFiles($form->getName()));
-    if ($form->isValid())
-    {
-      $page = $form->save();
-
-      $this->redirect('pages/edit?id='.$page->getId());
-    }
-  }
 }
---
+++ project/apps/backend/modules/pages/lib/pagesGeneratorHelper.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * pages module helper.
+ *
+ * @package    sf_sandbox
+ * @subpackage pages
+ * @author     Your name here
+ * @version    SVN: $Id: helper.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class pagesGeneratorHelper extends BasePagesGeneratorHelper
+{
+}
