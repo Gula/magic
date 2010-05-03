1484b6d91247c4065db06570c207e5b8ff312f70
RetroFOX <damian.suarez@xifox.net>
2010-05-02 22:16:03 -0300

    onLoad por onDomReady en main slideshow.

--- project/web/js/casino.js
+++ project/web/js/casino.js
@@ -1,4 +1,50 @@
 window.addEvent('load', function (ev) {
+  // Main Slideshow
+  var slideContainer = $('slideshow');
+
+  if(slideContainer) {
+    var slide_wrapper = slideContainer.getElement('.slide-wrapper'),
+    count_slides = slideContainer.getElements('.picture').length;
+
+
+    slide_wrapper.setStyles({
+      'width': count_slides*720
+    });
+
+    var slide = new Fx.Scroll(slideContainer, {
+      wait: false,
+      duration: 1000,
+      transition: Fx.Transitions.Quad.easeInOut
+    });
+
+    // auto
+    var moveSlide = function (ind) {
+      if (ind===undefined) {
+        if(this.ind == undefined) this.ind = 0;
+        this.ind = (this.ind > (count_slides - 1)) ? 0 : this.ind;
+      }
+      else {
+        this.ind = ind;
+      }
+
+      slide.start(720*this.ind, 0);
+      this.ind++;
+    }
+
+    var timer = moveSlide.periodical(10000);
+
+    // slideshow index
+    var slideIndex = $('slide-index');
+    slideIndex.addEvents({
+      click: function (ev) {
+        timer = $clear(timer);
+        timer = moveSlide.periodical(10000);
+        moveSlide(ev.target.get('text').toInt() -1);
+      }
+    })
+
+  } // end Main Slideshow
+
 });
 
 window.addEvent('domready', function (ev) {
@@ -26,53 +72,6 @@ window.addEvent('domready', function (ev) {
     display: 0,
     alwaysHide: false
   });
-
-  // Main Slideshow
-  var slideContainer = $('slideshow');
-
-  if(slideContainer) {
-    var slide_wrapper = slideContainer.getElement('.slide-wrapper'),
-    count_slides = slideContainer.getElements('.picture').length;
-
-    
-      slide_wrapper.setStyles({
-        'width': count_slides*720
-      });
-
-      var slide = new Fx.Scroll(slideContainer, {
-        wait: false,
-        duration: 1000,
-        transition: Fx.Transitions.Quad.easeInOut
-      });
-
-      // auto
-      var moveSlide = function (ind) {
-        if (ind===undefined) {
-          if(this.ind == undefined) this.ind = 0;
-          this.ind = (this.ind > (count_slides - 1)) ? 0 : this.ind;
-        }
-        else {
-          this.ind = ind;
-        }
-
-        slide.start(720*this.ind, 0);
-        this.ind++;
-      }
-
-      var timer = moveSlide.periodical(10000);
-
-      // slideshow index
-      var slideIndex = $('slide-index');
-      slideIndex.addEvents({
-        click: function (ev) {
-          timer = $clear(timer);
-          timer = moveSlide.periodical(10000);
-          moveSlide(ev.target.get('text').toInt() -1);
-        }
-      })
-    
-  } // end Main Slideshow
-
   
   
   // Carousel Page level 1
