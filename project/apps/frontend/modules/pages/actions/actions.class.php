4141ed78aa709eb066b0618ff69dfbb37870bf37
LauraMelo <laura.melo@xifox.net>
2010-05-29 20:20:13 -0300

    pagina vip

--- project/apps/frontend/modules/pages/actions/actions.class.php
+++ project/apps/frontend/modules/pages/actions/actions.class.php
@@ -17,98 +17,69 @@ class pagesActions extends sfActions
   */
   public function executeIndex(sfWebRequest $request)
   {
+    //Asignaciones
+    $this->vipNumber = 21;
     $this->level = $request->getParameter('level');
+    $this->currentPage = Doctrine::getTable('Page')->find($request->getParameter('id'));
+    $user = $this->getUser();
 
-    // paginas hijas
-    if($this->level == 1) {
-      // VIP
-      if($request->getParameter('id') == 21) {
-        $user = $this->getUser();
+    if ($user->isAuthenticated())
+      $this->isVip = $user->getGuardUser()->hasPermission('Vip');
+    else
+      $this->isVip = false;
+    
+    if($this->currentPage->getParent()->get('id') == $this->vipNumber and !($this->isVip))
+      return $this->redirect('pages/index?id='.$this->vipNumber.'&level=1'); 
+    else {
+      // paginas hijas
+      if($this->level == 1) {
+        // VIP
+        if($request->getParameter('id') == $this->vipNumber ) {
+          $class = sfConfig::get('app_sf_guard_plugin_signin_form', 'sfMooDooFormSignin');
+          $this->form = new $class();
+          if ($request->isMethod('post')) {
+            $this->form->bind($request->getParameter('signin'));
+            if ($this->form->isValid()) {
+              $values = $this->form->getValues();
+              $this->getUser()->signin($values['user'], array_key_exists('remember', $values) ? $values['remember'] : false);
+              return $this->redirect('pages/index?id='.$this->vipNumber.'&level=1');
+            }
+          } 
+        }// end login in page Vip
 
-        if ($user->isAuthenticated())
-        {
-          $this->isVip = $user->getGuardUser()->hasPermission('Vip');
-          //return $this->redirect('pages/index?id=21&level=1');
-        }
+        $this->childPage = Doctrine::getTable('Page')->find($request->getParameter('id'));
+        $this->page = $this->childPage->getParent();
 
-        $class = sfConfig::get('app_sf_guard_plugin_signin_form', 'sfMooDooFormSignin');
-        $this->form = new $class();
+        // Paginas hijas de show
+        if($this->childPage->getParent()->get('id') == 4) {
+          $catId = $this->childPage->get('id');
+          $catId = $catId - 26;
+          //$catId = $catId - 16;
 
-        if ($request->isMethod('post'))
-        {
-          $this->form->bind($request->getParameter('signin'));
-          if ($this->form->isValid())
-          {
-            $values = $this->form->getValues();
-            $this->getUser()->signin($values['user'], array_key_exists('remember', $values) ? $values['remember'] : false);
+          /* Asignacion de mierda
+          *
+          * 27 = main show   -> 4
+          * 28 = Belterra    -> 5
+          * 29 = Rainbow     -> 6
+          * 30 = Jocker      -> 7
+          */
 
-            // always redirect to a URL set in app.yml
-            // or to the referer
-            // or to the homepage
-            $signinUrl = '@vip'; //sfConfig::get('app_sf_guard_plugin_success_signin_url', $user->getReferer($request->getReferer()));
-
-            return $this->redirect('pages/index?id=21&level=1');
-          }
-        }
-        else
-        {
-          $user->setReferer($this->getContext()->getActionStack()->getSize() > 1 ? $request->getUri() : $request->getReferer());
-
-          $module = sfConfig::get('sf_login_module');
-          if ($this->getModuleName() != $module)
-          {
-            //return $this->redirect($module.'/'.sfConfig::get('sf_login_action'));
-          }
-          $this->getResponse()->setStatusCode(401);
+          $this->mainShow = EventTable::retrieveMainShow($catId);
+          $this->shows = EventTable::retrieveShows($catId);
+          $this->setTemplate('shows');
         }
       }
+        else {
+          $this->page = Doctrine::getTable('Page')->find($request->getParameter('id'));
+          $this->childPage = false;
+        }
 
-      // end login in pagfe id=21
-
-      $this->childPage = Doctrine::getTable('Page')->find($request->getParameter('id'));
-      $this->page = $this->childPage->getParent();
-
-      // Paginas hijas de show
-      if($this->childPage->getParent()->get('id') == 4) {
-        $catId = $this->childPage->get('id');
-        $catId = $catId - 26;
-        //$catId = $catId - 16;
-
-        /* Asignacion de mierda
-         * 
-         * 27 = main show   -> 4
-         * 28 = Belterra    -> 5
-         * 29 = Rainbow     -> 6
-         * 30 = Jocker      -> 7
-         */
-
-        $this->mainShow = EventTable::retrieveMainShow($catId);
-        $this->shows = EventTable::retrieveShows($catId);
+      // pagina de show
+      if($request->getParameter('id') == 4) {
+        $this->mainShow = EventTable::retrieveMainShow();
+        $this->shows = EventTable::retrieveShows();
         $this->setTemplate('shows');
       }
     }
-    else {
-      $this->page = Doctrine::getTable('Page')->find($request->getParameter('id'));
-      $this->childPage = false;
-    }
-
-    // pagina de show
-    if($request->getParameter('id') == 4) {
-      $this->mainShow = EventTable::retrieveMainShow();
-      $this->shows = EventTable::retrieveShows();
-      $this->setTemplate('shows');
-    }
   }
-   public function executeVip(sfWebRequest $request)
-  {
-    $user = $this->getUser();
-    if ($user->isAuthenticated() && $guard = $user->getGuardUser()->hasPermission('Vip'))
-    {
-      $this->setTemplate('shows');
-      echo "sos re vip";
-    }
-    else{
-      return $this->redirect('@sf_guard_signin');
-    }
-   }
 }
--- project/apps/frontend/modules/pages/templates/indexSuccess.php
+++ project/apps/frontend/modules/pages/templates/indexSuccess.php
@@ -4,11 +4,15 @@
   <div class="contenido">
     <div class="sub-menu">
       <h2><?php echo $page ?></h2>
+      <?php if ($sf_request->getParameter('id') == 21 and !($isVip)): ?>
+      
+      <?php else : ?>
       <ul>
         <?php foreach ($page->getChildren() as $child) : ?>
         <li><?php echo link_to($child->get('title'), 'pages/index?id='.$child->get('id').'&level=1') ?></li>
         <?php endforeach; ?>
       </ul>
+      <?php endif; ?>
     </div>
 
 
@@ -53,46 +57,31 @@
       <div class="subpagina-contenido">
         <h3><?php echo $realPage ?></h3>
         <?php echo $realPage->getRawValue()->getDescription() ?>
-      </div>
+      
 
       <?php if ($sf_request->getParameter('id') == 21 and !$sf_user->isAuthenticated()) : ?>
-      <div class="auth">
-        <form action="<?php echo url_for('pages/index') ?>" method="post">
-        <input type="hidden" value="21" name="id" />
-        <input type="hidden" value="1" name="level" />
-        <table>
+      
+        <form action="<?php echo url_for('pages/index#signin') ?>" method="post">
+        <p id="signin">
           <?php echo $form->renderGlobalErrors() ?>
-           <?php echo $form->renderHiddenFields() ?>
-           <tr>
-             <td>
-              <?php echo $form['username']->renderError() ?>
-              <?php echo $form['username'] ?>
-            </td>
-          </tr>
-           <tr>
-             <td>
-              <?php echo $form['password']->renderError() ?>
-              <?php echo $form['password'] ?>
-            </td>
-          </tr>
-       <tr>
-             <td>
-              <?php echo $form['remember']->renderError() ?>
-              <?php echo $form['remember']->renderLabel() ?>
-              <?php echo $form['remember'] ?>
-            </td>
-          </tr>
-
-
-        </table>
-
-        <input type="submit" value="<?php echo __('sign in') ?>" />
+          <input type="hidden" value="21" name="id" />
+          <input type="hidden" value="1" name="level" />
+          <?php echo $form->renderHiddenFields() ?>
+          <?php echo $form['username']->renderError() ?>
+          <?php echo $form['username'] ?>
+          <?php echo $form['password']->renderError() ?>
+          <?php echo $form['password'] ?>
+          <?php echo $form['remember']->renderError() ?>
+          <?php echo $form['remember']->renderLabel() ?>
+          <?php echo $form['remember'] ?>
+          <input type="submit" value="<?php echo __('sign in') ?>" />
+        </p>  
       </form>
 
-      </div>
-      <?php else: ?>
-      <?php echo link_to('Salir','@sf_guard_signout' ); ?>
+      <?php elseif ($sf_user->isAuthenticated()): ?>
+      <p><?php echo link_to('Salir','@sf_guard_signout' ); ?></p>
       <?php endif; ?>
+    </div>
     </div>
   </div>
 
