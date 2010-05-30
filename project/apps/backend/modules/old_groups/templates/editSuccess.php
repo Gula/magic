909a80efdd2d5884810d1f38ca20216f450342c0
Damian <damian.suarez@xifox.net>
2010-05-30 12:15:10 -0300

    Eliminamos modulos sfGuard desde el backend. Ya ... obsoletos.

--- project/apps/backend/modules/old_groups/templates/editSuccess.php
+++
@@ -1,3 +0,0 @@
-<h1>Edit Sf guard group</h1>
-
-<?php include_partial('form', array('form' => $form)) ?>
--- project/apps/backend/modules/old_permissions/templates/editSuccess.php
+++
@@ -1,3 +0,0 @@
-<h1>Edit Sf guard permission</h1>
-
-<?php include_partial('form', array('form' => $form)) ?>
